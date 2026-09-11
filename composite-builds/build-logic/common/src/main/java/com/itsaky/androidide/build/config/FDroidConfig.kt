04248e5118121a52ae856676b93d877ddbf8a88b
mahmo2005ud-lgtm <mahmo2005ud@gmail.com>
2026-09-11 05:33:07 +0200

    Restore missing build config source: FDroidConfig.kt

---
+++ composite-builds/build-logic/common/src/main/java/com/itsaky/androidide/build/config/FDroidConfig.kt
@@ -0,0 +1,53 @@
+package com.itsaky.androidide.build.config
+
+import org.gradle.api.Project
+import java.io.File
+
+object FDroidConfig {
+  var hasRead: Boolean = false
+    private set
+  var isFDroidBuild: Boolean = false
+    private set
+    get() = hasRead && field
+  var fDroidBuildArch: String? = null
+    private set
+  var fDroidVersionName: String? = null
+    private set
+  var fDroidVersionCode: Int? = null
+    private set
+  var aapt2Files: Map<String, String?> = emptyMap()
+    private set
+
+  const val PROP_FDROID_BUILD = "ide.build.fdroid"
+  const val PROP_FDROID_BUILD_ARCH = "ide.build.fdroid.arch"
+  const val PROP_FDROID_BUILD_VERSION = "ide.build.fdroid.version"
+  const val PROP_FDROID_BUILD_VERCODE = "ide.build.fdroid.vercode"
+  const val PROP_FDROID_AAPT2FILE_ARM64 = "ide.build.fdroid.aapt2File.arm64-v8a"
+  const val PROP_FDROID_AAPT2FILE_ARM = "ide.build.fdroid.aapt2File.armeabi-v7a"
+  const val PROP_FDROID_AAPT2FILE_X86_64 = "ide.build.fdroid.aapt2File.x86_64"
+
+  fun load(project: Project) {
+    val propsFile = File(project.rootDir, "fdroid.properties")
+    if (!propsFile.exists() || !propsFile.isFile) {
+      hasRead = true
+      isFDroidBuild = false
+      return
+    }
+
+    val properties = propsFile.let { props ->
+      java.util.Properties().also { it.load(props.reader()) }
+    }
+
+    hasRead = true
+    isFDroidBuild = properties.getProperty(PROP_FDROID_BUILD, null).toBoolean()
+    fDroidBuildArch = properties.getProperty(PROP_FDROID_BUILD_ARCH, null)
+    fDroidVersionName = properties.getProperty(PROP_FDROID_BUILD_VERSION, null)
+    fDroidVersionCode = properties.getProperty(PROP_FDROID_BUILD_VERCODE, null)?.toInt()
+
+    aapt2Files = mutableMapOf<String, String?>().also { files ->
+      files[PROP_FDROID_AAPT2FILE_ARM64.substringAfterLast('.')] = properties.getProperty(PROP_FDROID_AAPT2FILE_ARM64, null)
+      files[PROP_FDROID_AAPT2FILE_ARM.substringAfterLast('.')] = properties.getProperty(PROP_FDROID_AAPT2FILE_ARM, null)
+      files[PROP_FDROID_AAPT2FILE_X86_64.substringAfterLast('.')] = properties.getProperty(PROP_FDROID_AAPT2FILE_X86_64, null)
+    }
+  }
+}
